31b6b975117227e4dc4a381be09829e3dd83d8fa
Gustavo L Tamiosso <gustavotamiosso@DM004194.local>
2022-09-20 10:32:55 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solução para um desafio de aprendizado em pipelines e mlflow',
+    author='gustavo lopes tamiosso',
+    license='',
+)
